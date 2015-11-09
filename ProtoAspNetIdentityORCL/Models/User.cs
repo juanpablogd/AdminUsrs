a7f427e7ed8ee23928bd19b1476049ae578301c0
juanpablogd <juanpablogarzond@gmail.com>
2015-11-09 15:28:05 -0500

    Commit Inicial

---
+++ ProtoAspNetIdentityORCL/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NSPecor.Models
+{
+    public class User
+    {
+        public long ss_id_usr { get; set; }
+        public string ss_nombre_usr { get; set; }
+
+    }
+}
